f934339d9598b02dbcbd7ee526b6de396031d6ab
Patty <georgescu.patry@gmail.com>
2021-11-06 17:20:03 +0200

    Handle token expiry on delete posts, rename 'withAuthCheck' to 'withAuthInfo'

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,14 @@
 import express from 'express';
 
-import withAuthenticationCheck from '../middleware/withAuthenticationCheck.js';
+import withAuthenticationInfo from '../middleware/withAuthenticationInfo.js';
 import { getPosts, createPost, updatePost, deletePost, updateLikeCount } from '../controllers/postsController.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
-router.post('/new', withAuthenticationCheck, createPost);
-router.patch('/:id', withAuthenticationCheck, updatePost);
-router.delete('/:id', withAuthenticationCheck, deletePost);
-router.patch('/:id/update-likes', withAuthenticationCheck, updateLikeCount);
+router.post('/new', withAuthenticationInfo, createPost);
+router.patch('/:id', withAuthenticationInfo, updatePost);
+router.delete('/:id', withAuthenticationInfo, deletePost);
+router.patch('/:id/update-likes', withAuthenticationInfo, updateLikeCount);
 
 export default router;
